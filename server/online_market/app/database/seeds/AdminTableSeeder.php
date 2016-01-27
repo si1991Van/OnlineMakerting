b52fe708ac4c75d878541511937fc992ae7672a1
dxnm <2711sq@gmail.com>
2016-01-27 16:50:48 +0700

    fix conflict

--- server/online_market/app/database/seeds/AdminTableSeeder.php
+++ server/online_market/app/database/seeds/AdminTableSeeder.php
@@ -5,7 +5,6 @@ class AdminTableSeeder extends Seeder {
 	public function run()
 	{
 		Admin::create([
-				'role_id' => 1,
 				'email'=>'trantunghn196@gmail.com',
 				'password'=>Hash::make('123456'),
 				'username'=> 'tung1984',
