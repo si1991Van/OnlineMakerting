80ccf74cf37d73a47316b2a56c07f13d62ae7b0f
dxnm <2711sq@gmail.com>
2016-02-02 14:30:01 +0700

    fix fix

--- server/online_market/app/database/seeds/FavoriteTableSeeder.php
+++ server/online_market/app/database/seeds/FavoriteTableSeeder.php
@@ -5,18 +5,21 @@ class FavoriteTableSeeder extends Seeder {
 	public function run()
 	{
 		Favorite::create([
+			'type_favorite'=> '1',
 			'model_id'=> '1',
 			'model_name'=> 'Product',
 			'user_id' => '1',
 			'follow_id' => '2'
 		]);
 		Favorite::create([
+			'type_favorite'=> '1',
 			'model_id'=> '1',
 			'model_name'=> 'Product',
 			'user_id' => '1',
 			'follow_id' => '3'
 		]);
 		Favorite::create([
+			'type_favorite'=> '1',
 			'model_id'=> '2',
 			'model_name'=> 'Product',
 			'user_id' => '2',
