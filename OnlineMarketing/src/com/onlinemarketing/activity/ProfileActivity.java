e5fa6bac2c267dfdafebd6728fdad647a82f08e2
si1991Van <sivannguyen1991@gmail.com>
2016-02-23 09:10:15 +0700

    comit

---
+++ OnlineMarketing/src/com/onlinemarketing/activity/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.onlinemarketing.activity;
+
+import com.example.onlinemarketing.R;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.activity_profile);
+		super.onCreate(savedInstanceState);
+	}
+
+}
